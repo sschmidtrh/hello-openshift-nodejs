a5402594e8c40fa1793914eab93f6fe1d0f13a4a
sschmidtrh <sschmidt@redhat.com>
2017-06-14 13:13:11 +0200

    Create server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 8080;
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello from OpenShift to Credit Suisse\nAnd thanks to vbehar\n/steve\n");
+  console.log("Served another request");
 }).listen(port);
 
 console.log("Server running on port " + port);
