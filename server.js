4d9ec9ebba1fa9a42b24618e9e8266171e581c90
sschmidtrh <sschmidt@redhat.com>
2017-06-27 21:11:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello steve - from OpenShift!\nAnd thanks to vbehar.\n");
-  console.log("Served another request");
+  response.end("Hello Credit Suisse L&L - from OpenShift!\nAnd thanks to vbehar.\n");
+  console.log("Served another lunch");
 }).listen(port);
 
 console.log("Server running on port " + port);
