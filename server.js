a14ae2712101a5db6bef78036f905abe70f9b33b
sschmidtrh <sschmidt@redhat.com>
2018-03-29 08:00:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Production-Ready: Hello from OpenShift, steve\n");
-  console.log("Production-Ready: Served another request");
+  response.end("20180329 Accenture Workshop: Hello from OpenShift, steve\n");
+  console.log("20180329 Accenture Workshop: Served another request");
 }).listen(port);
 
 console.log("Server running on port " + port);
